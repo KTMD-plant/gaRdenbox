6da94f099706078b4ca39d50c0cab6019ba5adc2
KTMD-plant <kilian.duijts@gmail.com>
2021-05-17 17:17:25 +0200

    fixed wrong variable name in stringr function

--- R/gettairid.R
+++ R/gettairid.R
@@ -11,15 +11,16 @@
 #' AT followed by the chromosome number the gene is located on; 
 #' or alternatively a M for mitochondria or C for chloroplast if the gene is located on one of the plastid genomes.
 #' 
-#' @param string The text containing the gene identifiers.
-#' @return A list of Tair identifiers found in string.
+#' @param querytext The text containing the gene identifiers.
+#' @return A list of Tair identifiers found in the querytext
 #' @examples 
-#' ids<-gettairid(string=c("AT1G19030 AT3G61800,AT2G02200convallisAT5G23670,ATMG00730loreAT3G28291,AT4G07460,AT3G29ATMG00730"))
+#' ids<-gettairid(querytext=c("AT1G19030 AT3G61800,AT2G02200convallisAT5G23670,ATMG00730loreAT3G28291,AT4G07460,AT3G29ATMG00730"))
 #' 
 #' 
 #' 
-gettairid<-function(string){
-  ls_geneid<-stringr::str_extract_all(string = string,
+#' 
+gettairid<-function(querytext){
+  ls_geneid<-stringr::str_extract_all(querytext = querytext,
                              pattern =  
                                "AT(M|C|[:digit:]{1})(G{1})([:digit:]{5})")
   
