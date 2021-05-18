806c3ec229044e08841dd7b33ca48640291c6af8
KTMD-plant <kilian.duijts@gmail.com>
2021-05-18 09:39:16 +0200

    Expanded helpfile for get tair id

--- R/getTAIRid.R
+++ R/getTAIRid.R
@@ -5,16 +5,32 @@
 
 #dependancy
 
-#' Get tair identifier
+#' Get TAIR identifier
 #' 
 #' Extract arabidopsis (TAIR) gene identifiers from a text. These identifiers conform to the following structure:
-#' AT followed by the chromosome number the gene is located on; 
+#' 
+#' "AT" 
+#' followed by the chromosome number the gene is located on; 
+#' 
 #' or alternatively a M for mitochondria or C for chloroplast if the gene is located on one of the plastid genomes.
 #' 
-#' @param querytext The text containing the gene identifiers.
-#' @return A list of Tair identifiers found in the querytext
+#' "G" followed by 
+#' five-digit code, numbered from top/north to bottom/south of chromosome
+#' 
+#' Example of gene id: 
+#' nuclear: AT3G28291
+#' mitochondrial: ATMG00730
+#' 
+#' Does not extract: transposable elements (these do not follow this naming convention)
+#' For more info on TAIR ID nomenclature see https://www.arabidopsis.org/portals/nomenclature/guidelines.jsp
+#' 
+#' 
+
+#' 
+#' @param querytext The text (character string) containing the gene identifiers. Make sure the character string does not contain quotation marks within.
+#' @return A list of TAIR identifiers found in the querytext.
 #' @examples 
-#' ids<-gettairid(querytext=c("AT1G19030 AT3G61800,AT2G02200convallisAT5G23670,ATMG00730loreAT3G28291,AT4G07460,AT3G29ATMG00730"))
+#' ids<-gettairid(querytext="AT1G19030 AT3G61800,AT2G02200convallisAT5G23670,ATMG00730loreAT3G28291,AT4G07460,AT3G29ATMG00730"
 #' 
 #' 
 #' 
