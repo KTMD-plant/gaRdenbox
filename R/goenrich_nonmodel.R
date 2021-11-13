35a9a1975f00906d82f79c3095276937d2a42ac6
KTMD-plant <kilian.duijts@gmail.com>
2021-11-13 20:51:59 +0100

    docu

--- R/goenrich_nonmodel.R
+++ R/goenrich_nonmodel.R
@@ -3,9 +3,29 @@
 #Author: Kilian Duijts
 
 
+#' @title Gene Ontology testing for non-model species
+#' @description 
+#' Re-application of the goseq ("https://bioconductor.org/packages/release/bioc/html/goseq.html") package for use on non model species.
+#' This is based on a tutorial ("https://knozue.github.io/post/2018/10/17/over-representation-analysis-3-goseq-with-non-model-go-term.html") by knozue
+#' @details 
+#' The function makes it easier to test gene sets of non-model species.
+#' 
+#' It takes the following parameters:
+#' 
+#' @param querylist A list with for each element a non-model species gene id.
+#' @param padj Numerical value specifying the cut-off for the adjusted pvalue
+#' 
+#' # method <currently not available>
+#' 
+#' @param geneid2GO A named list for which the names are geneids and the list elements consist of GO-terms (like so: GO:0006355). Please make sure your geneid's are identical to the query genes.
+#' @param cDNA fasta file containing the non-model species geneid's and their sequences. The gene lenght is used for the probabily weight; normalization for gene length.
+#' 
+#' 
+
+
 
 non_model_goseq<-function(  #fuction runs goseq for non-modelspecies
-  genelist,                 #provide query list
+  querylist,                 #provide query list
   padj,                     #choose padj thresshold
   geneid2GO,                #provide named list. names=geneid, list elements goterms
   cdna                      #fasta file of sequences
@@ -26,25 +46,25 @@ non_model_goseq<-function(  #fuction runs goseq for non-modelspecies
   
   
   
-  #get genelist to test
+  #get querylist to test
   tgl<-as.integer(
-    names(bias_cdna) %in% genelist)
+    names(bias_cdna) %in% querylist)
   
   names(tgl)<-names(bias_cdna)
   
   
   #calculate probability weighting
-  pw<-nullp(tgl, bias.data = bias_cdna,plot.fit = TRUE)
+  pw<-goseq::nullp(tgl, bias.data = bias_cdna,plot.fit = TRUE)
   
   
-  goseq_out<-goseq(pwf = pw, gene2cat = geneid2go)
+  goseq_out<-goseq::goseq(pwf = pw, gene2cat = geneid2go)
   
   
   
   goseq_out[, "padj_BH"] <- p.adjust(goseq_out[, 2], method = "BH")
   
   GOenriched <- goseq_out[which(goseq_out[, "padj_BH"] < padj), ]
-  y <- genelist[genelist %in% names(geneid2go)]
+  y <- querylist[querylist %in% names(geneid2go)]
   
   if(nrow(GOenriched) != 0){
     GOenriched["Total"] <- nrow(pw)
@@ -57,7 +77,7 @@ non_model_goseq<-function(  #fuction runs goseq for non-modelspecies
     for (j in GOenriched$category) {
       
       a<-go2geneid[[j]]
-      gogenes<-a[a%in%genelist]%>%
+      gogenes<-a[a%in%querylist]%>%
         unlist()
       
       
